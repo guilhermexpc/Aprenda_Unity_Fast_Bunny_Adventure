836807f4a5b13489b137dc66287243b0eaf57164
Guilherme <guilherme1219@gmail.com>
2020-06-23 14:29:43 -0300

    Cenario Basico, Player Jump e Colisao

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rbPlayer;
+    private Vector2 vt2ForcaPulo;
+
+    [Header("Atributos")]
+    public float forcaPulo;
+
+    public Transform tfGroundCheck;
+    private bool grounded;
+
+    private void Awake()
+    {
+        rbPlayer = GetComponent<Rigidbody2D>();
+        vt2ForcaPulo = new Vector2(0, forcaPulo);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(tfGroundCheck.position, 0.02f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            rbPlayer.AddForce(vt2ForcaPulo);
+        }
+    }
+}
